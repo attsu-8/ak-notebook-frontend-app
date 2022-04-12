9e410f1bbdd72d7b7219b0bc935eb56ce7bc13c1
atsuzawa <atsuzawa.k8@gmail.com>
2022-04-12 12:55:45 +0900

    add: ユーザー登録画面の追加

---
+++ app/src/components/authentication/jwt-register.tsx
@@ -0,0 +1,162 @@
+import { VFC } from "react";
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { useMounted } from "../../hooks/use-mounted";
+import { fetchAsyncLogin, setIsAuthenticated, resetIsInitialized, setIsInitialized, fetchAsyncRegister, fetchAsyncCreateProf } from "../../slices/authentication/authSlice";
+import { useDispatch } from "react-redux";
+import { useRouter } from "next/router";
+import { TextField, Box, FormHelperText, Button} from "@mui/material";
+import {initializeStoreData} from "../../utils/load/initializeStoreData";
+
+export const JWTRegister: VFC = (props) => {
+    const isMounted = useMounted();
+    const dispatch = useDispatch();
+    const router = useRouter();
+
+    
+    const formik = useFormik({
+        initialValues: {
+            profileNickname: '',
+            userEmail: '',
+            password: '',
+            submit: null
+        },
+        validationSchema: Yup.object({
+            profileNickname: Yup
+                .string()
+                .max(30),
+            userEmail: Yup
+                .string()
+                .email('Must be a valid email')
+                .max(255)
+                .required('Email is required'),
+            password: Yup
+                .string()
+                .max(255)
+                .required('Password is required')
+        }),
+        onSubmit: async (values, helpers): Promise<void> => {
+            try {
+                const auth = {
+                    userEmail: values.userEmail,
+                    password: values.password,
+                }
+                let profile = {
+                    profileNickname: values.profileNickname
+                }
+
+                if (values.profileNickname === '') {
+                    profile = {
+                        profileNickname: 'anonymous'
+                    }
+                }
+
+                const result: any = await dispatch(fetchAsyncRegister(auth));
+                if (fetchAsyncRegister.fulfilled.match(result)) {
+
+                    const loginResult: any = await dispatch(fetchAsyncLogin(values));
+                    if (fetchAsyncLogin.fulfilled.match(loginResult)){
+                        await dispatch(resetIsInitialized());
+                        await dispatch(fetchAsyncCreateProf(profile));
+                        await dispatch(setIsAuthenticated());
+                        await initializeStoreData(dispatch);    
+                    }
+                }
+                
+                if (isMounted()) {
+                    const returnUrl = await (router.query.returnUrl as string) || '/';
+                    await router.push(returnUrl);
+                }
+
+                await dispatch(setIsInitialized());
+
+            } catch (err) {
+                console.error(err);
+
+                if (isMounted()) {
+                    helpers.setStatus({ success: false });
+                    helpers.setErrors({ submit: err.message });
+                    helpers.setSubmitting(false);
+                }
+            }
+        }
+    });
+
+    return (
+        <form
+            noValidate
+            onSubmit={formik.handleSubmit}
+            {...props}
+        >
+            <TextField
+                error={Boolean(
+                    formik.touched.profileNickname
+                    && formik.errors.profileNickname
+                )}
+                fullWidth
+                helperText={
+                    formik.touched.profileNickname
+                    && formik.errors.profileNickname}
+                label="Nickname"
+                margin="normal"
+                name="profileNickname"
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                value={formik.values.profileNickname}
+            />
+
+            <TextField
+                error={Boolean(
+                    formik.touched.userEmail
+                    && formik.errors.userEmail
+                )}
+                fullWidth
+                helperText={
+                    formik.touched.userEmail
+                    && formik.errors.userEmail}
+                label="Email Address"
+                margin="normal"
+                name="userEmail"
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                type="email"
+                value={formik.values.userEmail}
+            />
+
+            <TextField
+                error={Boolean(
+                    formik.touched.password
+                    && formik.errors.password
+                )}
+                fullWidth
+                helperText={formik.touched.password && formik.errors.password}
+                label="Password"
+                margin="normal"
+                name="password"
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                type="password"
+                value={formik.values.password}
+            />
+            {formik.errors.submit && (
+                <Box sx={{mt: 3}}>
+                    <FormHelperText error>
+                        {formik.errors.submit}
+                    </FormHelperText>
+                </Box>
+            )}
+            <Box sx={{ mt:2}}>
+                <Button
+                    disabled={formik.isSubmitting}
+                    fullWidth
+                    size="large"
+                    type="submit"
+                    variant="contained"
+                    color="secondary"
+                >
+                    Create
+                </Button>
+            </Box>
+        </form>
+    );
+};
---
+++ app/src/pages/authentication/register.tsx
@@ -0,0 +1,108 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import { Box, Card, Container, Divider, Link, Typography } from '@mui/material';
+import { Logo } from '../../components/logo';
+import NextLink from 'next/link';
+import { JWTRegister } from '../../components/authentication/jwt-register';
+
+const Login: NextPage = () => {
+
+    return (
+    <>
+        <Head>
+            <title>
+                Create Account | ak-notebook
+            </title>
+        </Head>
+
+        <Box
+            component="main"
+            sx={{
+                backgroundColor: 'background.default',
+                display: 'flex',
+                flexDirection: 'column',
+                minHeight: '100vh'
+            }}
+        >
+            <Container
+                maxWidth="sm"
+                sx={{
+                py: {
+                    xs: '60px',
+                    md: '120px'
+                }
+                }}
+            >
+                <Card
+                elevation={16}
+                sx={{ p: 4 }}
+                >
+                    <Box
+                        sx={{
+                        alignItems: 'center',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center'
+                        }}
+                    >
+                        <Logo
+                            sx={{
+                                m:1,
+                                height: 40,
+                                width: 40,
+                            }}
+                        />
+                        <Typography variant="h4">
+                            Create Account
+                        </Typography>
+                        <Typography
+                            color="textSecondary"
+                            sx={{ mt: 2 }}
+                            variant="body2"
+                        >
+                            アカウント情報を入力してください
+                        </Typography>
+                        <Typography
+                            color="textSecondary"
+                            variant="body2"
+                        >
+                            ※ ニックネームは省略可能
+                        </Typography>
+                    </Box>
+                    <Box
+                        sx={{
+                        flexGrow: 1,
+                        mt: 3
+                        }}
+                    >
+                        <JWTRegister />
+                    </Box>
+                    <Divider sx={{ my: 3 }} />
+                    <Box
+                        sx={{
+                            mr: 1,
+                            display: "flex",
+                            justifyContent: "flex-end",
+                        }}
+                    >
+                        <NextLink
+                            href={'/authentication/login'}
+                        >
+                            <Link
+                                color="textSecondary"
+                                variant="body1"
+                                underline='always'
+                                sx={{cursor: "pointer"}}
+                            >
+                                アカウントをお持ちの方
+                            </Link>
+                        </NextLink>
+                    </Box>
+                </Card>
+            </Container>
+        </Box>
+    </>
+    );
+    };
+      
+    export default Login;
