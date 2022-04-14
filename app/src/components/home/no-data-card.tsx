9ab4267cfeab9d0e4d1efb013acfe1075f595522
atsuzawa <atsuzawa.k8@gmail.com>
2022-04-15 02:38:47 +0900

    add: メモ０件時の描画改善

---
+++ app/src/components/home/no-data-card.tsx
@@ -0,0 +1,26 @@
+import { Card, Typography } from "@mui/material";
+import { VFC } from "react";
+
+
+export const NoDataCard: VFC = () => {
+
+    return (
+        <Card
+            sx={{
+                display: 'flex',
+                alignItems: 'center',
+                width:"100%",
+                height: "100px",
+                p:1,
+            }}
+        >
+            <Typography
+                color="textSecondary"
+                variant="body1"
+            >
+                メモを作成してください。
+            </Typography>
+
+        </Card>
+    );
+}
