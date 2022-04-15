aebd684b1e3f53195ae520420506013e6239ef45
atsuzawa <atsuzawa.k8@gmail.com>
2022-04-16 00:30:23 +0900

    update: メモ別学習効率の日本語対応

--- app/src/components/home/select-memo-card.tsx
+++ app/src/components/home/select-memo-card.tsx
@@ -2,11 +2,13 @@
 
 
 import { VFC } from "react";
-import { Box, Card,CardActions } from "@mui/material";
+import { Box, Card,CardActions, Typography } from "@mui/material";
 import type { Memo } from "../../types/memo/memo";
 import { MemoCardTitle } from "../memo/memo/memo-card-title";
 import { MemoCardPriority } from "../memo/memo/memo-card-priority";
 import { MemoCardContent } from "../memo/memo/memo-card-content";
+import { useSelector } from "react-redux";
+import { selectSelectChildMemoCategoryName } from "../../slices/home/learningEfficiencySlice";
 
 
 interface SelectMemoCardProps {
@@ -17,6 +19,7 @@ interface SelectMemoCardProps {
 
 export const SelectMemoCard: VFC<SelectMemoCardProps> = (props) => {
     const { memo, onClickDeletePropertyButton, ...other} = props;
+    const selectChildMemoCategoryName = useSelector(selectSelectChildMemoCategoryName)
     
     return (
         <>
@@ -24,7 +27,6 @@ export const SelectMemoCard: VFC<SelectMemoCardProps> = (props) => {
             <Box
                 sx={{
                     outline: 'none',
-                    py: 1,
                     width: "100%"
                 }}
                 {...other}
@@ -36,28 +38,48 @@ export const SelectMemoCard: VFC<SelectMemoCardProps> = (props) => {
                         disableSpacing
                         sx={{
                             display: "flex",
-                            alignItems: "center",
+                            flexDirection: "column",
                         }}
                     >
                         <Box
                             sx={{
-                                mb:2,
-                                mr:5,
-                                width:"70%"
+                                mb:5,
+                                ml:1,
+                                width: "100%"
                             }}
                         >
-                            <MemoCardTitle memo={memo} />
+                            <Typography
+                                color="textSecondary"
+                                variant="h6"
+                            >
+                                {`子カテゴリ名：${selectChildMemoCategoryName}`}
+                            </Typography>
                         </Box>
 
                         <Box
                             sx={{
-                                mb:5,
-                                width:"30%"
+                                display: "flex",
+                                alignItems: "senter",
+                                width: "100%"
                             }}
                         >
-                            <MemoCardPriority memo={memo} />
-                        </Box>
+                            <Box
+                                sx={{
+                                    mr:5,
+                                    width:"70%"
+                                }}
+                            >
+                                <MemoCardTitle memo={memo} />
+                            </Box>
 
+                            <Box
+                                sx={{
+                                    width:"30%"
+                                }}
+                            >
+                                <MemoCardPriority memo={memo} />
+                            </Box>
+                        </Box>
                     </CardActions>
 
                     <MemoCardContent memo={memo} />
